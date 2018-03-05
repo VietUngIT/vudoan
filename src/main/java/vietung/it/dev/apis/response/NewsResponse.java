9bd92ccbb5b73c73f61420adf347948c8560026d
VietUngIT <vietungit140494@gmail.com>
2018-03-05 23:38:34 +0700

    handler news

---
+++ src/main/java/vietung/it/dev/apis/response/NewsResponse.java
@@ -0,0 +1,24 @@
+package vietung.it.dev.apis.response;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import lombok.Data;
+
+@Data
+public class NewsResponse extends BaseResponse {
+    private JsonObject data;
+    private JsonArray datas;
+    @Override
+    public String toJonString() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("e",getError());
+        jsonObject.addProperty("msg",getMsg());
+        if(data!=null){
+            jsonObject.add("data",data);
+        }
+        if(datas!=null){
+            jsonObject.add("array",datas);
+        }
+        return jsonObject.toString();
+    }
+}
--- src/main/java/vietung/it/dev/core/models/News.java
+++ src/main/java/vietung/it/dev/core/models/News.java
@@ -1,5 +1,6 @@
 package vietung.it.dev.core.models;
 
+import com.google.gson.JsonObject;
 import lombok.Data;
 import org.bson.Document;
 
@@ -14,6 +15,7 @@ public class News extends MongoLog{
     private String source;
     private String tags;
     private int views;
+    private int likes;
     private int comments;
     private long timeCreate;
     private String typeNews;
@@ -22,15 +24,36 @@ public class News extends MongoLog{
         Document document = new Document();
         document.append("title",title);
         document.append("shortDescription",shortDescription);
-        document.append("content",content);
         document.append("author",author);
         document.append("image",image);
         document.append("source",source);
         document.append("tags",tags);
         document.append("views",views);
+        document.append("likes",likes);
         document.append("comments",comments);
         document.append("typeNews",typeNews);
         document.append("timeCreate",timeCreate);
+        document.append("content",content);
         return document;
+    }
+
+    public JsonObject toJson(){
+        JsonObject jsonObject = new JsonObject();
+        if(_id!=null){
+            jsonObject.addProperty("id",_id);
+        }
+        jsonObject.addProperty("title",title);
+        jsonObject.addProperty("shortDescription",shortDescription);
+        jsonObject.addProperty("author",author);
+        jsonObject.addProperty("image",image);
+        jsonObject.addProperty("source",source);
+        jsonObject.addProperty("tags",tags);
+        jsonObject.addProperty("views",views);
+        jsonObject.addProperty("likes",likes);
+        jsonObject.addProperty("comments",comments);
+        jsonObject.addProperty("typeNews",typeNews);
+        jsonObject.addProperty("timeCreate",timeCreate);
+        jsonObject.addProperty("content",content);
+        return jsonObject;
     }
 }
